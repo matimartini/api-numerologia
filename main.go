e37d29e5dd1fcc21932e16f064acd54c664ce576
Matias Gabriel Martini <matias.martini.gabriel@gmail.com>
2020-05-21 20:50:01 -0300

    agregado de gin-gonic test ping/pong test-1

--- main.go
+++ main.go
@@ -5,9 +5,35 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"github.com/gin-gonic/gin"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
+
+func main() {
+	route := gin.Default()
+
+	route.GET("/", ping)
+	route.GET("/ping", ping)
+
+
+	router.Run(":" + port)
+}
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func hello(c *gin.Context){
+	io.WriteString(w, "Hello Word")
+	c.JSON(200, gin.H{
+		"message": "Hello Word!!!",
+	})
+}
+
+// function without  gin-gonic
+/*func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello Word")
 	fmt.Println("Hello Word!!!!")
 }
@@ -16,4 +42,4 @@ func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":"+port, nil)
-}
+}*/
